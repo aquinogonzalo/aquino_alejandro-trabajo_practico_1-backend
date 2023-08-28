6509e4f415c8a5eef4591bc317c8ef9db60125b2
aquinogonzalo <aquinoalejandro666@gmail.com>
2023-08-27 22:19:41 -0300

    feat: modelar tabla usuarios

---
+++ src/models/Usuario.js
@@ -0,0 +1,50 @@
+//Importamos sequelize y DataTypes
+const { DataTypes, sequelize } = require('../database');
+
+//Modelo de Usuario
+const Usuario = sequelize.define('usuario', {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+    },
+    nombre: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    correo: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    contraseña: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    createdAt: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: sequelize.literal('CURRENT_TIMESTAMP')
+    },
+    updatedAt: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: sequelize.literal('CURRENT_TIMESTAMP')
+    },
+    deletedAt: {
+        type: DataTypes.DATE,
+        allowNull: true
+    }
+}, {
+    createdAt: true,
+    updatedAt: true,
+    deletedAt: true,
+    tableName: 'usuarios'
+}
+);
+//Esto hay que cambiar de false a true, si es que queremos crear la tabla si es que no habia creado.
+Usuario.sync({ force: false }).then(() => {
+    console.log('Hey, la tabla usuarios se ha creado!');
+});
+
+
+module.exports = Usuario
